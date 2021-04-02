475568f1e7375f3f49fac8d997ec78e6cc82df42
伊秦轩-Cloud-java <yiqinxuan2012@163.com>
2021-04-02 18:40:26 +0800

    feat(tdd): rollback GildedRose

--- src/main/java/com/kingmao/tdd/gildedrose/Item.java
+++ src/main/java/com/kingmao/tdd/gildedrose/Item.java
@@ -1,6 +1,5 @@
 package com.kingmao.tdd.gildedrose;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,100 +16,10 @@ public class Item {
 
     public int quality;
 
-    public static Item createBackstagePass(int sellIn, int quality) {
-        return new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
-    }
-
-    public static Item createNormalItem(String msg ,int sellIn, int quality) {
-        return new Item(msg, sellIn, quality);
-    }
-
-    public static Item createSulfuras(int sellIn, int quality) {
-        return new Item("Sulfuras, Hand of Ragnaros", sellIn, quality);
-    }
-
-    private Item(String name, int sellIn, int quality) {
+    protected Item(String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
 
-    public boolean isAgedBrie() {
-        return name.equals("Aged Brie");
-    }
-
-    public boolean isEqualsSulfuras() {
-        return name.equals("Sulfuras, Hand of Ragnaros");
-    }
-
-    public boolean isEqualsBackstatge() {
-        return name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
-    void updateQualityByOneDay() {
-        updateQuality();
-        updateSellIn();
-
-
-        if (isExpired()) {
-            updateByAfterExpiredtion();
-        }
-    }
-
-    private void updateQuality() {
-        if (!isAgedBrie()
-                && !isEqualsBackstatge()) {
-            if (quality > 0) {
-                if (!isEqualsSulfuras()) {
-                    quality = quality - 1;
-                }
-            }
-        } else {
-            if (quality < 50) {
-                quality = quality + 1;
-
-                if (isEqualsBackstatge()) {
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private void updateByAfterExpiredtion() {
-        if (!isAgedBrie()) {
-            if (!isEqualsBackstatge()) {
-                if (quality > 0) {
-                    if (!isEqualsSulfuras()) {
-                        quality = quality - 1;
-                    }
-                }
-            } else {
-                quality = 0;
-            }
-        } else {
-            if (quality < 50) {
-                quality = quality + 1;
-            }
-        }
-    }
-
-    private boolean isExpired() {
-        return sellIn < 0;
-    }
-
-    private void updateSellIn() {
-        if (!isEqualsSulfuras()) {
-            sellIn = sellIn - 1;
-        }
-    }
 }
--- src/test/java/com/kingmao/tdd/gildedrose/TextTestFixture.java
+++ src/test/java/com/kingmao/tdd/gildedrose/TextTestFixture.java
@@ -1,9 +1,8 @@
 package com.kingmao.tdd.gildedrose;
 
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
-import static com.kingmao.tdd.gildedrose.Item.*;
 
 /**
  * @author QX
@@ -22,16 +21,16 @@ public class TextTestFixture {
         printStream.println("OMGHAI!");
 
         Item[] items = new Item[] {
-                createNormalItem("+5 Dexterity Vest", 10, 20), //
-                createNormalItem("Aged Brie", 2, 0), //
-                createNormalItem("Elixir of the Mongoose", 5, 7), //
-                createSulfuras(0, 80), //
-                createSulfuras( -1, 80),
-                createBackstagePass( 15, 20),
-                createBackstagePass( 10, 49),
-                createBackstagePass( 5, 49),
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 // this conjured item does not work properly yet
-                createNormalItem("Conjured Mana Cake", 3, 6) };
+                new Item("Conjured Mana Cake", 3, 6) };
 
         GildedRose app = new GildedRose(items);
 
@@ -44,7 +43,7 @@ public class TextTestFixture {
                 printStream.println(item);
             }
             printStream.println();
-            app.passOneDay();
+            app.updateQuality();
         }
         return out.toString();
     }
